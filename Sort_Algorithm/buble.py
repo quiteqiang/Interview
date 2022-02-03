950305b1501eb420d995e818e101fc04baeb7fd4
quiteqiang <qiang_wang@taylor.edu>
2022-02-03 10:48:49 +0800

    Titile

---
+++ Sort_Algorithm/buble.py
@@ -0,0 +1,17 @@
+def bublesort(arr):
+    n = len(arr)
+
+    for i in range(n -1):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j + 1], arr[j]
+
+
+#driver code
+arr = [1,7,9,2,4]
+
+bublesort(arr)
+
+print ("Sorted array is:")
+for i in range(len(arr)):
+    print ("%d" %arr[i])
